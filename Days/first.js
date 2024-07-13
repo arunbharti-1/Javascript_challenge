fca7f3d75c2e68310ef28298c6f56ddd568b77b3
arunbharti-1 <114228660+arunbharti-1@users.noreply.github.com>
2024-07-13 17:06:33 +0000

    first

---
+++ Days/first.js
@@ -0,0 +1,40 @@
+// Activity 1: Variable declaration
+"use strict"
+/*
+var password = 12345678
+console.log(password)
+let email = "arun@gmail.com"//always prefer to use let don't use var bcoz it has a scope problem
+console.log(email)
+
+const id = 212121
+//id=2 const cannot be reassign   
+//terminal me ye likhna hai < $ node Days/first.js >
+console.log(id)
+*/
+// Data type
+let name = "Arun"
+let age = 22
+let isGraduate = true
+let state 
+console.log(name)
+console.log(age)
+console.log(isGraduate)
+console.log(state)
+
+console.log(typeof name)
+
+console.log(typeof age)
+
+console.log(typeof isGraduate)
+
+console.log(typeof state)
+console.log(typeof null)
+
+
+
+
+
+
+
+
+
